1cd6094e43a8631fcbb4a06d68ce28af47938301
dimits <tsirbasdim@gmail.com>
2024-05-09 14:33:46 +0300

    Add pseudo label history and add period in pseudolabeling

--- tasks/adaptive_train_eval.py
+++ tasks/adaptive_train_eval.py
@@ -62,6 +62,7 @@ def train_adaptive_model(
         target_val_dataset: tasks.data.ImageDataset,
         output_dir: str,
         num_epochs: int = 25,
+        pseudo_sample_period: int = 1,
         rho=3,
         previous_source_history: dict[str, list[float]] = None,
         previous_target_history: dict[str, list[float]] = None,
@@ -159,28 +160,29 @@ def train_adaptive_model(
                 device=device,
             )
 
-        # ========= Pseudo-labeling task =========
-        threshold = adaptive_threshold(classification_accuracy=last_val_acc, rho=rho)
-        samples = select_samples(
-            model,
-            unlabeled_dataloader_initializer(unlabeled_target_train_dataset),
-            threshold,
-            device,
-        )
+        if epoch % pseudo_sample_period == 0:
+            # ========= Pseudo-labeling task =========
+            threshold = adaptive_threshold(classification_accuracy=last_val_acc, rho=rho)
+            samples = select_samples(
+                model,
+                unlabeled_dataloader_initializer(unlabeled_target_train_dataset),
+                threshold,
+                device,
+            )
 
-        print(
-            f"Selected {len(samples[0])}/{len(unlabeled_target_train_dataset)} images on threshold {threshold}"
-        )
+            print(
+                f"Selected {len(samples[0])}/{len(unlabeled_target_train_dataset)} images on threshold {threshold}"
+            )
 
-        pseudo_label_history.append([])
-        for image_path, class_id in zip(samples[0], samples[1]):
-            # update datasets and recreate dataloaders
-            unlabeled_target_train_dataset.remove(image_path)
-            pseudolabeled_target_train_dataset.add(image_path, class_id)
+            pseudo_label_history.append([])
+            for image_path, class_id in zip(samples[0], samples[1]):
+                # update datasets and recreate dataloaders
+                unlabeled_target_train_dataset.remove(image_path)
+                pseudolabeled_target_train_dataset.add(image_path, class_id)
 
-            # update pseudo label history
-            pseudo_label_history[epoch].append((image_path, class_id))
-        print(pseudo_label_history[epoch])
+                # update pseudo label history
+                pseudo_label_history[epoch].append((image_path, class_id))
+            print(pseudo_label_history[epoch])
 
         # ========= Target domain forward and backward pass =========
         target_dataloaders = {
