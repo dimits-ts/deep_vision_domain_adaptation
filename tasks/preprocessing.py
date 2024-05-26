1d6dc8bc3241e630a05c9e5042312d0580b49a2b
dimits <tsirbasdim@gmail.com>
2024-05-26 12:42:16 +0300

    solve deprecation issue

--- tasks/preprocessing.py
+++ tasks/preprocessing.py
@@ -45,7 +45,8 @@ def resnet_preprocessor(image: np.ndarray) -> torch.Tensor:
     :rtype: numpy.ndarray
     """
     preprocess = v2.Compose([
-        v2.ToTensor(),
+        v2.ToImage(),
+        v2.ToDtype(torch.float32, scale=True),
         v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
